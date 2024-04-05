26b10162cd782f13c8a30cbbf47c4d934807227f
Mikas Dulevicius <mikas1213@gmail.com>
2024-04-05 19:37:20 +0300

    making mobile navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation, useNavigate } from "react-router-dom";
 
 import styles from "./Navbar.module.css";
 import { Logo } from "./Logo";
@@ -9,16 +9,17 @@ import Authentication from "../auth/Authentication";
 import useAuth from "../../hooks/useAuth";
 import useLogout from '../../hooks/useLogout';
 
-// import { FaVideo } from "react-icons/fa6";
 import { FaPhotoVideo } from "react-icons/fa";
 import { FaClipboardList } from "react-icons/fa6";
 import { FaHeadset } from "react-icons/fa6";
 import { FaUser } from "react-icons/fa6";
-
 import { FaPowerOff } from "react-icons/fa6";
 import { FaArrowRightToBracket } from "react-icons/fa6";
 
 const Navbar = ({ isHome = false }) => {
+    const location = useLocation();
+    const navigate = useNavigate();
+
     const logout = useLogout();
     
     const [isScroll, setIsScroll] = useState(false);
@@ -90,36 +91,47 @@ const Navbar = ({ isHome = false }) => {
 
                 <div className={`${styles.navContainerMobile} ${isOpenBurger ? styles.show : ''}`}>
                     <ul className={styles.navListMobile}>
-                        
-                            <li className={styles.listItemMobile}>  
-                                <FaPhotoVideo className={styles.icon}/>
-                                <NavLink to="/virtuve">Virtuvė</NavLink>
-                            </li>
-                        
-                        
-                            <li className={styles.listItemMobile}>
-                                <FaClipboardList className={styles.icon}/>
-                                <NavLink to="/receptai">Receptai</NavLink>
-                            </li>
+
+                        <li className={`${styles.listItemMobile} ${location.pathname.indexOf('virtuve') > -1 ? styles.active : ''}`} 
+                            onClick={() => navigate('/virtuve')}>  
+                            <FaPhotoVideo className={styles.icon}/>
+                            <span>Virtuvė</span>
+                        </li>
                         
                         
-                            <li className={styles.listItemMobile}>
-                                <FaHeadset className={styles.icon}/>
-                                <NavLink to="/paslaugos">Paslaugos</NavLink>
-                            </li>
+                        <li className={`${styles.listItemMobile} ${location.pathname.indexOf('receptai') > -1 ? styles.active : ''}`} 
+                            onClick={() => navigate('/receptai')}>
+                            <FaClipboardList className={styles.icon}/>
+                            <span>Receptai</span>
+                        </li>
                         
                         
-                            <li className={styles.listItemMobile}>
-                                <FaUser className={styles.icon}/>
-                                <NavLink to="/profilis">Profilis</NavLink>
-                            </li>
+                        <li className={`${styles.listItemMobile} ${location.pathname.indexOf('paslaugos') > -1 ? styles.active : ''}`}
+                            onClick={() => navigate('/paslaugos')}>
+                            <FaHeadset className={styles.icon}/>
+                            <span>Paslaugos</span>
+                        </li>
                         
+
+                        {auth?.accessToken && <li className={`${styles.listItemMobile} ${location.pathname.indexOf('profilis') > -1 ? styles.active : ''}`}
+                            onClick={() => navigate('/profilis')}>
+                            <FaUser className={styles.icon}/>
+                            <span>Profilis</span>
+                        </li>}
                         
-                            <li className={`${styles.listItemMobile} ${styles.signout}`}>
-                                <FaPowerOff className={styles.icon}/>
-                                <a>Atsijungti</a>
-                            </li>
-                        
+                            
+                        {!auth.accessToken ? <li 
+                            className={`${styles.listItemMobile} ${styles.signout}`}
+                            onClick={() => setIsOpenModal(true)}>
+                            <FaArrowRightToBracket className={styles.icon}/>
+                            <span>Prisijungti</span>
+                        </li>
+                        :
+                        <li className={`${styles.listItemMobile} ${styles.signout}`}
+                            onClick={async () => await logout()}>
+                            <FaPowerOff className={styles.icon}/>
+                            <span>Atsijungti</span>
+                        </li>}
                     </ul>
                 </div>
             </nav>
