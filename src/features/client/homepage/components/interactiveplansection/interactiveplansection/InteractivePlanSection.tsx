b13456ee3ab21b1b5fa908a32df4d0c79e5bf497
Mikas <mikas1213@gmail.com>
2025-10-24 17:21:35 +0300

    scrollend => scroll [dev]

--- src/features/client/homepage/components/interactiveplansection/interactiveplansection/InteractivePlanSection.tsx
+++ src/features/client/homepage/components/interactiveplansection/interactiveplansection/InteractivePlanSection.tsx
@@ -79,44 +79,10 @@ export const InteractivePlanSection = () => {
             const cardWidth = cardElement.offsetWidth;
             const scrollLeft = container.scrollLeft;
             const index = Math.round(scrollLeft / cardWidth);
-
-            
             setSelected(index % allCards.length);
-
-
             const singleSetWidth = container.scrollWidth / 3;
-            console.log("puse ekrano: ", window.innerWidth / 2);
-            console.log('scrollLeft: ', scrollLeft);
-            console.log('cardWidth: ', cardWidth);
-            console.log('- - - - - - - - - - - - -\n');
-			// if (scrollLeft <= 10) {
-				
-			// 	isScrollingRef.current = true;
-			// 	container.scrollLeft = singleSetWidth + scrollLeft;
-			// 	setTimeout(() => {
-			// 		isScrollingRef.current = false;
-			// 	}, 50);
-			// }
 
-			// if (scrollLeft >= singleSetWidth * 2 - 10) {
-				
-			// 	isScrollingRef.current = true;
-			// 	container.scrollLeft =
-			// 		singleSetWidth + (scrollLeft - singleSetWidth * 2);
-			// 	setTimeout(() => {
-			// 		isScrollingRef.current = false;
-			// 	}, 50);
-			// }
-        };
-
-        const handleScroll2 = () => {
-             const cardElement = container.querySelector('[class*=interactiveCard]') as HTMLElement;
-            if (!cardElement) return;
-            const singleSetWidth = container.scrollWidth / 3;
-            const cardWidth = cardElement.offsetWidth;
-            const scrollLeft = container.scrollLeft;
-            
-            			if (scrollLeft <= 10) {
+			if (scrollLeft <= 10) {
 				
 				isScrollingRef.current = true;
 				container.scrollLeft = singleSetWidth + scrollLeft;
@@ -135,8 +101,8 @@ export const InteractivePlanSection = () => {
 				}, 50);
 			}
         };
-        container.addEventListener('scroll', handleScroll2);
-        container.addEventListener('scrollend', handleScroll);
+        
+        container.addEventListener('scroll', handleScroll);
         return () => container.removeEventListener('scroll', handleScroll);
     }, []);
 
