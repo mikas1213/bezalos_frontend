afd1a92d8981b76cf88bd95f1262e59c770478be
Mikas <mikas1213@gmail.com>
2025-10-25 16:44:43 +0300

    mute Admin [prod]

--- src/layouts/ClientLayout.tsx
+++ src/layouts/ClientLayout.tsx
@@ -13,7 +13,7 @@ const ClientLayout = () => {
 
     const mainStyles = {
         backgroundColor: 'var(--white-100)',
-        minHeight: '85dvh',
+        minHeight: '100vh',
         paddingTop: location.pathname === '/receptai' ? '0' : mediaQuery < 441 ? 'var(--nav-height-60)' : mediaQuery < 577 ? 'var(--nav-height-70)' : 'var(--nav-height-80)'
     } as CSSProperties
 
