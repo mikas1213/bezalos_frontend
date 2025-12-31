7233b3494f38faa6530531c720fa92f71cc769e8
Mikas <mikas1213@gmail.com>
2025-12-31 16:53:48 +0200

    change email counts [dev]

--- src/pages/admin/MailsPage.jsx
+++ src/pages/admin/MailsPage.jsx
@@ -1,13 +1,15 @@
 import { useState, useEffect } from 'react';
 import useAxiosPrivate from '../../hooks/useAxiosPrivate';
 import toast from 'react-hot-toast';
+import { useOutletContext } from 'react-router-dom';
 
 const MailsPage = () => {
 
     const axiosPrivate = useAxiosPrivate();
     const [emails, setEmails] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
-
+    const { stats } = useOutletContext();
+    console.log('stats: ', Object.entries(stats.email_stats))
     useEffect(() => {
         const getData = async () => {
             try {
@@ -23,7 +25,15 @@ const MailsPage = () => {
     }, [axiosPrivate]);
     return (
         <>
-            <span>Total: {emails.length}</span>
+            <div style={{ marginBottom: '1rem', display: 'flex', gap: '1rem'}}>
+                {
+                    Object.entries(stats.email_stats).map(([source, count]) => <div>
+                        <span>{source}</span>:&nbsp;
+                        <span style={{ fontWeight: 'bold'}}>{count}</span>
+                    </div>
+                )}
+            </div>
+
             {!isLoading && emails.map(email => {
                 return (<p key={email.id}>{email.email} {email.created_at.split('T').shift()}</p>);
             })}
