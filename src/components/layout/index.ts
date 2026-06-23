1c5528f2f8c91003d640e9fb3119cc2489984c64
Mikas <mikas1213@gmail.com>
2026-06-23 14:02:22 +0300

    fix: .nav padding-block-end: var(--s-8)

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,5 +1,5 @@
 export { Banner } from './Banner/Banner';
 export { CookieConsent } from './cookieconsent/CookieConsent';
 export { Footer } from './footer/footer/Footer';
-export { Navbar } from './Navbar/Navbarr';
+export { Navbar } from './Navbar/Navbar';
 export { Offer } from './offer/Offer';
